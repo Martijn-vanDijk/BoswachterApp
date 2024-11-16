49b6df083940a341133f8aeddd9f72236c8c24d7
THINKR\sona <sona.hyblova@gmail.com>
2024-11-16 14:25:07 +0100

    update observation and users

--- app/models/users.py
+++ app/models/users.py
@@ -2,17 +2,11 @@ from sqlmodel import Field, SQLModel, Column
 from enum import Enum as TypeEnum
 from sqlmodel import Enum as DatabaseEnum
 
-class UserBase(SQLModel):
-    """
-    This is the base class for Observation entities containing all fields the
-    user can set.
-    """
-    user_name : str
-    password : str
-    
-class User(UserBase, table=True):
+class User(SQLModel, table=True):
     """
     This is the table representation for the Observation entity, it includes
     all fields from the base class and our auto increment ID.
     """
     id: int | None = Field(default=None, primary_key=True)
+    user_name : str
+    password : str
