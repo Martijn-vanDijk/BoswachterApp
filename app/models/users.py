d454dac2e888dffee715777e50cf8f6765f3832e
Martijn van Dijk <mpavandijk@gmail.com>
2024-11-16 15:38:20 +0100

    reverted

--- app/models/users.py
+++ app/models/users.py
@@ -2,7 +2,7 @@ from sqlmodel import Field, SQLModel, Column
 from enum import Enum as TypeEnum
 from sqlmodel import Enum as DatabaseEnum
 
-class User(SQLModel, table = True):
+class User(SQLModel):
     """
     This is the table representation for the Observation entity, it includes
     all fields from the base class and our auto increment ID.
@@ -10,10 +10,3 @@ class User(SQLModel, table = True):
     user_name : str
     password : str
     id: int | None = Field(default=None, primary_key=True)
-    
-
-class CreateUser(User):
-    user_name : str
-    password : str
-
-    pass
--- app/routers/users.py
+++ app/routers/users.py
@@ -4,48 +4,30 @@ from sqlmodel import select, Session
 from app.dependencies.database import engine, add_to_session
 from typing import Annotated
 
+from fastapi import HTTPException, Query, APIRouter
+from app.models.users import *
+from sqlmodel import select, Session
+from app.dependencies.database import engine, add_to_session
+from typing import Annotated
+
 router = APIRouter(
     prefix="/users",
     tags=["users"],
 )
 
-# @router.post("/Register", status_code=201)
-# def __make_user(user : CreateUser) -> User:
-#     with Session(engine) as session:
-#         db_users = User.model_validate(user)
-#         add_to_session(db_users, session)
-#         return db_users
-#         db_observation = Observation.model_validate(observation)
-#         add_to_session(db_observation, session)
-#         return db_observation
-
-@router.post("/", status_code=201)
-def __post_user(user: CreateUser) -> User:
+@router.post("/Register")
+def __make_user(name: str, password :str):
     with Session(engine) as session:
-        db_users = Observation.model_validate(user)
-        add_to_session(db_users, session)
-        return db_users
-
-@router.get("/")
-def __get_users(
-    offset: int = 0,
-    limit: Annotated[int, Query(le=100)] = 100,
-) -> list[User]:
+        User.user_name = name
+        User.password = password
+        return "Success"
+    
+@router.get("/user")
+def __get_user(name: str, password: str):
     with Session(engine) as session:
-        users = session.exec(select(User).offset(offset).limit(limit)).all()
-        return users
-
-@router.get("/{id}")
-def __get_user(id: int) -> User:
-    with Session(engine) as session:
-        return __require_user(id, session)
-
-# @router.get("/user")
-# def __get_user(name: str, password: str):
-#     with Session(engine) as session:
-#         IsUser = False
-#         if (User.user_name == name) and (User.password == password):
-#             IsUser = True
+        IsUser = False
+        if (User.user_name == name) and (User.password == password):
+            IsUser = True
         
-#         return IsUser
+        return IsUser
         
